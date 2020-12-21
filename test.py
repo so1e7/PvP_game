6d9257d58ddfb04701f4ae39dfab3cc547ec07fb
daniilmorozov233 <76431755+daniilmorozov233@users.noreply.github.com>
2020-12-21 17:43:51 +0300

    Update test.py

--- test.py
+++ test.py
@@ -6,7 +6,13 @@ import pygame
 
 
 class GameTests(unittest.TestCase):
-    
+   
+    @patch('game.stop_game')
+    def test_game_start(self, test_patch):
+        """Проверяем работает ли остановка игры при health = 0"""
+        test_patch.return_value = 'win_test' 
+        game.health2 = 0
+       self.assertEqual(game.run_game(), 'win_test')
     @patch('pygame.key.get_pressed')
     @patch('game.pause_game')
     def test_exit_game(self, patch_pause, patch_keys):
